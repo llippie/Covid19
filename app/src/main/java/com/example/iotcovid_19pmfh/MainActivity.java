29f46c4ed379792671ebe0516fb8064d3575fdfa
llippie <syafie_tugiman2@yahoo.com>
2022-07-27 19:30:51 +0800

    graph tukar health info

--- app/src/main/java/com/example/iotcovid_19pmfh/MainActivity.java
+++ app/src/main/java/com/example/iotcovid_19pmfh/MainActivity.java
@@ -1,23 +1,21 @@
 package com.example.iotcovid_19pmfh;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.BeginSignInResult;
-import com.google.android.gms.auth.api.identity.Identity;
-import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -27,22 +25,27 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQ_ONE_TAP = 2;
-    private static final String TAG = "Tag-name";
-    private SignInClient oneTapClient;
-    private BeginSignInRequest signInRequest;
-
+    private static final String CHANNEL_ID = "Notification";
     String[] user = {"User1", "User2", "User3", "User4"};
     TextView temp, spo2, rate, status;
     Button btnTograph, refresh;
-    int spo, tem, heart;
-    //Spinner spin = (Spinner) findViewById(R.id.spinner);
+    ImageView icon;
+    int spo, heart;
+    double tem;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //notification channel
+        NotificationChannel channel = new NotificationChannel("Notification", "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(channel);
+
+        //top bar title
         Objects.requireNonNull(getSupportActionBar()).setTitle("Home");
 
         // Write a message to the database
@@ -53,50 +56,16 @@ public class MainActivity extends AppCompatActivity {
         spo2 = findViewById(R.id.spo2);
         rate = findViewById(R.id.rate);
         status = findViewById(R.id.status);
+        icon = findViewById(R.id.icon);
         refresh = findViewById(R.id.button1);
         btnTograph = findViewById(R.id.button2);
 
-        //spin.setOnItemSelectedListener(this);
-        //ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, user);
-        //spin.setAdapter(aa);
-
-        oneTapClient = Identity.getSignInClient(this);
-        signInRequest = BeginSignInRequest.builder()
-                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                        .setSupported(true)
-                        .build())
-                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                        .setSupported(true)
-                        // Your server's client ID, not your Android client ID.
-                        .setServerClientId(getString(R.string.default_web_client_id))
-                        // Only show accounts previously used to sign in.
-                        .setFilterByAuthorizedAccounts(true)
-                        .build())
-                // Automatically sign in when exactly one credential is retrieved.
-                .setAutoSelectEnabled(true)
-                .build();
-
-        oneTapClient.beginSignIn(signInRequest)
-                .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
-                    @Override
-                    public void onSuccess(BeginSignInResult result) {
-                        try {
-                            startIntentSenderForResult(
-                                    result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
-                                    null, 0, 0, 0);
-                        } catch (IntentSender.SendIntentException e) {
-                            Log.e(TAG, "Couldn't start One Tap UI: " + e.getLocalizedMessage());
-                        }
-                    }
-                })
-                .addOnFailureListener(this, new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // No saved credentials found. Launch the One Tap sign-up flow, or
-                        // do nothing and continue presenting the signed-out UI.
-                        Log.d(TAG, e.getLocalizedMessage());
-                    }
-                });
+        //notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_danger)
+                .setContentTitle("Low oxygen level!")
+                .setContentText("Please go to nearest medical centre for check-up")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         btnTograph.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,13 +78,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 name(dbRef);
+
             }
+
         });
 
         name(dbRef);
 
     }
-
 
     private void name(DatabaseReference dbRef) {
         dbRef.child("User1").child("heartrate").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
@@ -148,14 +118,22 @@ public class MainActivity extends AppCompatActivity {
 
                                             try {
                                                 spo = Integer.parseInt(spo2.getText().toString().trim());
-                                                tem = Integer.parseInt(temp.getText().toString().trim());
+                                                tem = Double.parseDouble(temp.getText().toString().trim());
                                                 heart = Integer.parseInt(rate.getText().toString().trim());
 
                                                 if (spo <= 94) {
                                                     status.setText("Danger");
-                                                    //rate.setText((CharSequence) spo2);
+                                                    icon.setImageResource(R.drawable.ic_danger);
+                                                    //NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+                                                    //notificationManager.notify(1, builder.build());
                                                 } else {
-                                                    status.setText("Good");
+                                                    if ((heart > 60 && heart < 100) && (tem >= 36 && tem < 38)) {
+                                                        status.setText("Good");
+                                                    }
+                                                    else {
+                                                        status.setText("Caution");
+                                                    }
+
                                                 }
                                             } catch (Exception e) {
                                                 return;
